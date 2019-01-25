76cd9119096db1212b1e7c982ec531be2b9639ef
hjp-github <525596082@qq.com>
2019-01-25 15:06:22 +0800

    测试Git

---
+++ src/main/java/com/hjp/cloud/MyApplication.java
@@ -0,0 +1,15 @@
+package com.hjp.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class MyApplication {
+    public static void main( String[] args ){
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
---
+++ src/main/java/com/hjp/cloud/MyController.java
@@ -0,0 +1,29 @@
+package com.hjp.cloud;
+
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello world";
+	}
+	
+	@RequestMapping(value = "/person/{personId}", method = RequestMethod.GET
+			, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findPerson(@PathVariable("personId") Integer personId) {
+		Person person = new Person();
+		person.setId(personId);
+		person.setName("胡际鹏");
+		person.setAge(18);
+		return person;
+	}
+	
+}
